0ea70cc483d1f714262cdbb8dc73dc0700b3995f
Juan Diego Elissalde Gudefín <118232153+Juan-Diego-E@users.noreply.github.com>
2024-07-15 00:25:19 +0000

    Updated ToDo list data with API

--- src/js/component/todo.jsx
+++ src/js/component/todo.jsx
@@ -9,15 +9,7 @@ const ToDo = () => {
 
 
     // Inicio fincionalidad Cambio modal -----------------------------------
-    let loginEvent = "show"
-    let todoEvent = "dont-show"
-
-    function changeModal(e) {
-        if(e.type == "click"){
-            loginEvent = "dont-show"
-            todoEvent = "show"
-        }
-    }
+    const [opened, setOpened] = useState(true);
 
     // Inicio fincionalidad To-Do -----------------------------------
 
@@ -61,55 +53,62 @@ const ToDo = () => {
                 }
             })
             .catch(error => console.log(error))
+        setOpened(false)
     }
 
     useEffect(() => {
         getData();
     }, []);
 
-    return (
+    return opened ? (
         <>
-            <button type="button" className="btn btn-primary" onChange={changeModal}>Primary</button>
-            <div className={loginEvent}>
+            <div className="show">
                 <h1 className="text-center">Enter username</h1>
                 <form className="d-flex justify-content-center flex-column" onSubmit={(e) => {
                     e.preventDefault()
                     getData()
                 }}>
                     <input onChange={(e) => setUsername(e.target.value)} value={username} type="text" className="form-control mt-3 mb-2" placeholder="Type here..." />
-                    <button onClick={() => getData()} type="submit" className="btn btn-primary">Create</button>
+                    <button onClick={() => getData()} type="submit" className="btn btn-primary my-4">Change</button>
                 </form>
             </div>
-            <div className={todoEvent}>
-                <h3 className="wellcome">Wellcome {username}!</h3>
-                <h1 className="text-center fw-light">To-Do</h1>
-                <div className="card w-12">
-                    <ul className="list-group list-group-flush">
-                        <div className="input-group d-flex">
-                            <input className="list-group-item" type="text" placeholder="Type here..." value={tarea} onKeyDown={agregarTarea} onChange={(e) => setTarea(e.target.value)} />
-                            <button className="btn list-group-item" type="button" onClick={agregarTarea} >Add</button>
-                        </div>
-                        {listaTareas && listaTareas.length > 0 ? listaTareas.map((tarea, index) => (
-                            <div className="input-group border border-0 rounded-0" key={index}>
-                                <div className="input-group-text rounded-0">
-                                    <input className="form-check-input" type="checkbox"></input>
-                                </div>
-                                <li className="list-group-item todo-item form-control rounded-0" key={index}>{tarea.label}
-                                    <button
-                                        type="button" className="btn-close remove-btn" aria-label="Close"
-                                        onClick={() => setListaTareas(listaTareas.filter((t, currentIndex) => index !== currentIndex))}>
-                                    </button>
-                                </li>
-                            </div>
-                        )) :
-                            <label className="list-group-item m-auto text-secondary">No pending tasks</label>
-                        }
-                        <label className="list-group-item m-auto text-secondary">{`${listaTareas.length} task left`}</label>
-                    </ul>
-                </div>
-            </div>
         </>
-    );
-};
+    )
+        :
+        (
+            <>
+
+                <div className="show">
+                    <h3 className="wellcome">Wellcome {username}!</h3>
+                    <h1 className="text-center fw-light">To-Do</h1>
+                    <div className="card w-12">
+                        <ul className="list-group list-group-flush">
+                            <div className="input-group d-flex">
+                                <input className="list-group-item" type="text" placeholder="Type here..." value={tarea} onKeyDown={agregarTarea} onChange={(e) => setTarea(e.target.value)} />
+                                <button className="btn list-group-item" type="button" onClick={agregarTarea} >Add</button>
+                            </div>
+                            {listaTareas && listaTareas.length > 0 ? listaTareas.map((tarea, index) => (
+                                <div className="input-group border border-0 rounded-0" key={index}>
+                                    <div className="input-group-text rounded-0">
+                                        <input className="form-check-input" type="checkbox"></input>
+                                    </div>
+                                    <li className="list-group-item todo-item form-control rounded-0" key={index}>{tarea.label}
+                                        <button
+                                            type="button" className="btn-close remove-btn" aria-label="Close"
+                                            onClick={() => setListaTareas(listaTareas.filter((t, currentIndex) => index !== currentIndex))}>
+                                        </button>
+                                    </li>
+                                </div>
+                            )) :
+                            <label className="list-group-item m-auto text-secondary">No pending tasks</label>
+                            }
+                            <label className="list-group-item m-auto text-secondary">{`${listaTareas.length} task left`}</label>
+                        </ul>
+                    </div>
+                <button className="btn btn-secondary my-5" type="button" onClick={() => setOpened(true)}>Change user</button>
+                </div>
+            </>
+        );
+    };
 
 export default ToDo;
